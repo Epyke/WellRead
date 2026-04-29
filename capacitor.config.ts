e506fbea6679801973b172ab7d7151e1c0490a0a
Epyke <epicsniper278@gmail.com>
2026-04-29 22:11:36 +0100

    init tabs template ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wellread',
+  webDir: 'www'
+};
+
+export default config;
